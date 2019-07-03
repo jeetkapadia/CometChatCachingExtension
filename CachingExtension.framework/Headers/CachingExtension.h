9325f8059643f11bcf4e7466ba663ff0716b1bb6
INSCRIPTS <JEET@INSCRIPTSs-Mac-mini-2.local>
2019-07-03 16:58:04 +0530

    Initial Release

---
+++ CachingExtension.framework/Headers/CachingExtension.h
@@ -0,0 +1,19 @@
+//
+//  CachingExtension.h
+//  CachingExtension
+//
+//  Created by Admin on 15/03/1941 Saka.
+//  Copyright © 1941 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CachingExtension.
+FOUNDATION_EXPORT double CachingExtensionVersionNumber;
+
+//! Project version string for CachingExtension.
+FOUNDATION_EXPORT const unsigned char CachingExtensionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CachingExtension/PublicHeader.h>
+
+
